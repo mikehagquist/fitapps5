556f7a90670f1fb517fe4761e9a5ca7a31c41f50
mikehagquist <mikehagquist@gmail.com>
2019-03-20 15:45:18 -0400

    Added email confirmation page.

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,30 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    person = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    height = models.SmallIntegerField()
+    birthdate = models.DateField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def createperson(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.person
+
+
+class Goals(models.Model):
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    goalname = models.CharField(max_length=60)
+    startdate = models.DateField()
+    enddate = models.DateField()
+    startingweight = models.SmallIntegerField()
+    targetweight = models.SmallIntegerField()
+    activitylevel = models.SmallIntegerField
+
+    def __str__(self):
+        return self.goalname
